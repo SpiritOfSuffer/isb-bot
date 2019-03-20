8c99a0d8170ac98c95564d5e20d57f033e3db18e
Top Deuse <topdeuse@gmail.com>
2019-03-20 10:30:46 +0300

    bodyparser fix

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(bodyParser.json())
+
+app.use(cors());
 
 app.post('/api/callback/approve', (req, res) => {
 
