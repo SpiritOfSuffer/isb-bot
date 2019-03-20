c2ee6815ad828334ce1bff5e38ac21cf3daabf73
Top Deuse <topdeuse@gmail.com>
2019-03-20 10:10:49 +0300

    added bodyParser urlencoding

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import request from 'request';
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json())
 
 app.post('/api/callback/approve', (req, res) => {
