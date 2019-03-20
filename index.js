372e53b13e036f8ae34bed26678b1159f077d39f
Top Deuse <topdeuse@gmail.com>
2019-03-20 10:01:32 +0300

    test

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ app.post('/api/callback/approve', (req, res) => {
     }
     if(req.body.type === "message_new") {
         let text = req.body.text;
-        if(text.indexOf("настя инфа") !== -1) {
+        console.log(text);
+        /*if(text.indexOf("настя инфа") !== -1) {
             const message = `Вероятность составляет: ${randomInteger(0, 100)}%`;
             const url = `https://api.vk.com/method/messages.send?chat_id=2&message=${message}&random_id=${Math.ceil(Math.random()*100000000)}&access_token=c5f0c9862f1d6e72d2296b710eb62914730426c11bcd3ea2e787d81d1eb6f329173aebca67228e32ec96f&v=5.92`;
             request.post(encodeURI(url));
-        }
+        }*/
         res.status(200).send('ok')
     }
 
-    console.log(req.body);
+    //console.log(req.body);
 });
 
 app.get('/api/callback/approve', (req, res) => {
