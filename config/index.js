562c8fee1d475e6b9535986c60c8e06538cf1861
Top Deuse <topdeuse@gmail.com>
2019-06-04 16:54:41 +0300

    update config

--- config/index.js
+++ config/index.js
@@ -7,9 +7,9 @@ export class BotConfig {
     }
 }
 
-export const groupId = 179504857;
-export const responseString = "3a4e1fdb";
-export const accessToken = "7478c7761bf43d803b6d4a97746bae5283ee34a97444e3d088f925cca1b209246da843541375806a8cc49";
+export const groupId = 179812415; //179504857
+export const responseString = "4d0fcb53"; //3a4e1fdb
+export const accessToken = "c5f0c9862f1d6e72d2296b710eb62914730426c11bcd3ea2e787d81d1eb6f329173aebca67228e32ec96f"; //7478c7761bf43d803b6d4a97746bae5283ee34a97444e3d088f925cca1b209246da843541375806a8cc49
 export const openWeatherMapToken = "5cc14094aa1c87dc004054e08a9d4927";
 export const appealPhrases = ["настя", "настенька", "настюша", "насть", "настюха"];
 export const commands = ["инфа", "чат", "кто", "позвать всех", "погода", "рифма", "дать кличку", "клички"];
