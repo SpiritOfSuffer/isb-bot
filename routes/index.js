5f6b3d2a01d48d5746b1ee231316387663b7f57b
Top Deuse <topdeuse@gmail.com>
2019-06-04 22:20:21 +0300

    a

--- routes/index.js
+++ routes/index.js
@@ -317,7 +317,7 @@ router.post('/api/callback/approve', async (req, res) => {
                     console.log(admins); 
                     console.log(from_id);
                     console.log(admins.includes(parseInt(from_id)));
-                    if(admins.includes(from_id)) {
+                    if(admins.includes(parseInt(from_id))) {
                         console.log(process.env.HOME);
                         const greeting = text.split(' ').splice(2).join(' ');
                         let obj = {
